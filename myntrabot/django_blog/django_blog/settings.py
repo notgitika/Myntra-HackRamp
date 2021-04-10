e24546932cf47ef0504276b2d9b9cdb7e948028e
unknown <manashreejhawar@gmail.com>
2021-04-10 18:13:01 +0530

    Chatbot included

--- myntrabot/django_blog/django_blog/settings.py
+++ myntrabot/django_blog/django_blog/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'bot/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
